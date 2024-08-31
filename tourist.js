4e64350e1babb8252e1e03fff8562951e35af6c0
unknown <sr0589071@gmail.com>
2024-08-31 20:16:29 +0600

    create tourist.js

---
+++ tourist.js
@@ -0,0 +1,20 @@
+
+// 1. Declare an array of 3 tourist destinations
+let touristDestinations =["Switzerland","New York","Paris"];
+console.log(touristDestinations);
+
+// 2. Add a new tourist destination to your tourist array
+
+  touristDestinations.push("Pakistan");
+  console.log(touristDestinations);
+
+ 3. //Add two more tourist destinations to your array
+  touristDestinations.push("Rome","Saudi Arabia")
+ console.log(touristDestinations);
+
+4. //Remove the last tourist destination you have added
+ touristDestinations.pop("Paris");
+ console.log(touristDestinations);
+
+ //5. display the final array as output
+ console.log(touristDestinations);
