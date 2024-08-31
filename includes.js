d0244b9a211eae48f602379700e88d1bdf675f58
unknown <sr0589071@gmail.com>
2024-08-31 21:31:20 +0600

    added an includes.js file

---
+++ includes.js
@@ -0,0 +1,15 @@
+//1. Create an array of books containing different book.
+let books =["To kill a Mockinbird","1984","Moby-Dick","JavScript: The Good Parts","The Great Gatsby"];
+// console.log(books);
+
+
+//2. Use the includes method to check if the array contains a javascript book.
+// console.log(books.includes('JavScript: The Good Parts'));
+
+// 3. Print a message to the console indicating whether the element is present in the array or not.
+     if(books.includes){
+         console.log("the array contains a javascript book");
+     }
+    else{
+        console.log("the array does not contains a javascript book");
+    }
