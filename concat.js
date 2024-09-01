230062fee95982b2fe44201b62bcc5ca3ead9c21
unknown <sr0589071@gmail.com>
2024-09-01 09:05:03 +0600

    added an concat.js

---
+++ concat.js
@@ -0,0 +1,18 @@
+// 1. Create two arrays
+var array1 = [1, 2, 3];
+var array2 = [4, 5, 6];
+
+console.log(array1);
+console.log(array2);
+//2. Use the concat method to combine the two arrays into a new array.
+
+ var result = array1.concat(array2);
+ console.log(result);
+
+    combinedArray =[1, 2, 3, 4, 5, 6]
+    console.log(combinedArray);
+ 
+//3. Print both the original arrays and the combined array using console.log().
+  console.log("Array 1:", array1);
+  console.log("Array 2:", array2);
+  console.log("Combined Array:", combinedArray);
